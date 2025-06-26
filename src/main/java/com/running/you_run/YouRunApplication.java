8eb2c2da723d71ac06314d39a40d2697f707f236
Zepelown <dbswjdqhd12@gmail.com>
2025-06-26 17:45:39 +0900

    feat: 스프링 기본 파일

---
+++ src/main/java/com/running/you_run/YouRunApplication.java
@@ -0,0 +1,13 @@
+package com.running.you_run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouRunApplication.class, args);
+	}
+
+}
