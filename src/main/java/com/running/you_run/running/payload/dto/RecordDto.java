2c674b9e10b3c63e368f0de56ebfef9fcf93ef4f
yechan928 <cksdid1363@gmail.com>
2025-07-10 17:56:31 +0900

    feature: 트랙에서 기록 저장시 사용자가 실제로 이동한 거리 추가 #15

--- src/main/java/com/running/you_run/running/payload/dto/RecordDto.java
+++ src/main/java/com/running/you_run/running/payload/dto/RecordDto.java
@@ -1,6 +1,8 @@
 package com.running.you_run.running.payload.dto;
 
 import com.running.you_run.running.entity.Record;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.running.you_run.running.util.CoordinateConverter;
 
 import java.util.List;
 
@@ -10,5 +12,9 @@ public record RecordDto(
 ) {
     public static RecordDto from(Record record,TrackInfoDto trackInfoDto){
         return new RecordDto(record,trackInfoDto);
+    }
+    @JsonProperty("userPath")
+    public List<CoordinateDto> userPath() {
+        return CoordinateConverter.convertLineStringToCoordinates(record.getPath());
     }
 }
