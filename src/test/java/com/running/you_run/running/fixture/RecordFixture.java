f7835ba9d4da5e2c2c4b6032e8ff2c8e6417f03d
Zepelown <dbswjdqhd12@gmail.com>
2025-07-12 00:44:51 +0900

    feat: 경로 단순화 로직 추가

--- src/test/java/com/running/you_run/running/fixture/RecordFixture.java
+++ src/test/java/com/running/you_run/running/fixture/RecordFixture.java
@@ -5,13 +5,13 @@ import com.running.you_run.running.payload.request.RecordStoreRequest;
 import java.time.LocalDateTime;
 
 public class RecordFixture {
-    public static RecordStoreRequest createRecordRequest(Long userId, Long trackId, long distance, double resultTimeSeconds) {
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime finished = now.plusSeconds((long) resultTimeSeconds);
-
-        return new RecordStoreRequest(
-                userId, "BOT", trackId, null, null,
-                now, finished, 0.0, distance
-        );
+//    public static RecordStoreRequest createRecordRequest(Long userId, Long trackId, long distance, double resultTimeSeconds) {
+//        LocalDateTime now = LocalDateTime.now();
+//        LocalDateTime finished = now.plusSeconds((long) resultTimeSeconds);
+//
+//        return new RecordStoreRequest(
+//                userId, "BOT", trackId, null, null,
+//                now, finished, 0.0, distance
+//        );
     }
 }
