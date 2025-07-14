73c2dcbf603b8f5ac3ca7b6ea4c5706dfc1b5969
Zepelown <dbswjdqhd12@gmail.com>
2025-07-15 01:11:22 +0900

    feat: 한국 시간대로 지정

--- src/main/java/com/running/you_run/YouRunApplication.java
+++ src/main/java/com/running/you_run/YouRunApplication.java
@@ -1,10 +1,19 @@
 package com.running.you_run;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class YouRunApplication {
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println("Date "+ new Date());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(YouRunApplication.class, args);
