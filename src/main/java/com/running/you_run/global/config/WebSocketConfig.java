3769ca8c264224f88d29c98ca4b2b30eec905c5c
xlouisjungx <cutaandcrazy@gmail.com>
2025-07-18 03:57:41 +0900

    Fix WebSocket

--- src/main/java/com/running/you_run/global/config/WebSocketConfig.java
+++ src/main/java/com/running/you_run/global/config/WebSocketConfig.java
@@ -1,21 +1,22 @@
 package com.running.you_run.global.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    private WebSocketHandler myWebSocketHandler;
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
-    }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic"); // 구독 prefix
-        registry.setApplicationDestinationPrefixes("/app"); // 클라이언트 보낼 때 prefix
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myWebSocketHandler, "/ws")
+                .setAllowedOrigins("*");
     }
 }
